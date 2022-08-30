d7678c9104d956360efb029867f14feed46279db
Murat <Muratsahh_61@outlook.com>
2022-08-30 18:28:45 +0300

    .

--- Core/Entities/BaseEntity.cs
+++ Core/Entities/BaseEntity.cs
@@ -9,6 +9,7 @@ namespace Core.Entities
     public  class BaseEntity
     {
         public Guid Id { get; set; }
+        public DateTime CreatedDate { get; set; }
 
     }
 }
--- DataAccess/Concrete/Context.cs
+++ DataAccess/Concrete/Context.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Core.Entities;
+using Entities.Abstract.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,11 +10,33 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete
 {
-    public class Context : IdentityDbContext
+    public class Context : IdentityDbContext<AppUser,AppRole,string>
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server = .;Database=ChatApp;Integrated Security=True;");
+        }
+        public DbSet<Entities.Abstract.Chat> Chats { get; set; }
+        public DbSet<Entities.Abstract.Message> Messages { get; set; }
+        public DbSet<Entities.Abstract.ChatMember> ChatMembers { get; set; }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            //ChangeTracker : Entityler üzerinden yapılan değişiklerin ya da yeni eklenen verinin yakalanmasını sağlayan propertydir. Update operasyonlarında Track edilen verileri yakalayıp elde etmemizi sağlar.
+
+            var datas = ChangeTracker
+                 .Entries<BaseEntity>();
+
+            foreach (var data in datas)
+            {
+                _ = data.State switch
+                {
+                    EntityState.Added => data.Entity.CreatedDate = DateTime.UtcNow,
+                    _ => DateTime.UtcNow
+                };
+            }
+
+            return await base.SaveChangesAsync(cancellationToken);
         }
     }
 }
